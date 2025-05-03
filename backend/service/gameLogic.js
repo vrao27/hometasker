84c4b02681ce3fdc8b1630ffec006a7c30bd2ce4
Vinay Rao <vrao8912@gmail.com>
2025-05-03 18:02:28 +0200

    upated block 1 in the gameLogic

--- backend/service/gameLogic.js
+++ backend/service/gameLogic.js
@@ -2,7 +2,12 @@ const Task = require('../models/task');
 const User = require('../models/user');
 
 
+//The game logic has 2 main blocks: 
+//Block 1  - Deals with finding the user and task and awarding points
+//Block 2 - Deals with the leaderboard and ranking the users based on their points
 
+
+//Block 1
 //Game logic steps using the async function - mark a task as completed by a user
 
 async function completeTask(userId, taskId) { 
@@ -21,11 +26,18 @@ async function completeTask(userId, taskId) {
     // Save the change back to MongoDB.
     await task.save();
 
+    //Step 3: find the user and award points for the completed task
+    const user = await User.findById(userId);
+    if (!user) {
+        throw new Error('User not found'); //error handling if no user is found
+    }
 
+    //if the user is new, default the value to 0 and then add the points
+    user.points = (user.points || 0) + task.points;
+    // Save the change back to MongoDB.
+    await user.save();
 
-
-
-
-
+    //Step 4: return the updated results
+    return { task, user };
 
 }

cfdaff0248b1616f39c8accfc96709a363ea47bd
Vinay Rao <vrao8912@gmail.com>
2025-05-22 22:24:15 +0200

    imported app

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import app from "./app";
 
 // Start the server
+const app = require('./app');
+
+
 const PORT = process.env.PORT || 5000; // Use PORT from .env or default to 5000
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);

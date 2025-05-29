085df8e787f989fbfa056dcf0cbe8079efa13f08
Vinay Rao <vrao8912@gmail.com>
2025-05-29 22:38:44 +0200

    rename eslint file

--- frontend-v2/eslint.config.js
+++ frontend-v2/eslint.config.js
@@ -1,32 +1,40 @@
-// eslint.config.js
 import js from '@eslint/js';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 import reactPlugin from 'eslint-plugin-react';
 import globals from 'globals';
 
 export default [
   js.configs.recommended,
   {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaFeatures: { jsx: true }
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin
+    }
+  },
+  {
     files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.es2021
-      },
-      parserOptions: {
-        ecmaFeatures: { jsx: true },
-        ecmaVersion: 'latest',
-        sourceType: 'module'
       }
     },
     plugins: {
-      react: reactPlugin,
-      '@typescript-eslint': tsPlugin
+      react: reactPlugin
     },
     rules: {
       'react/jsx-uses-react': 'off',
       'react/react-in-jsx-scope': 'off',
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-unused-vars': 'off' // Disable in favor of TypeScript version
     }
   }
 ];

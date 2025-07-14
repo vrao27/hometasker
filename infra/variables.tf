02ece1dd5f9bc185e9ddb228fd0366ab3a48556c
Vinay Rao <vrao8912@gmail.com>
2025-07-14 17:29:44 +0200

    add domain name variable

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,3 +3,8 @@ variable "region" {
   type        = string
   default     = "eu-central-1"
 }
+
+variable "domain_name" {
+  description = "The domain to manage in Lightsail DNS (e.g. hometasker.org)"
+  type        = string
+}

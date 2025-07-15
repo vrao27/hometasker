ee71c357b8d5ff302ed6425bac9b98e8ef92f7ea
Vinay Rao <vrao8912@gmail.com>
2025-07-15 20:16:36 +0200

    created docker-setup script to install docker and docker compose

--- infra/main.tf
+++ infra/main.tf
@@ -14,8 +14,9 @@ resource "aws_lightsail_instance" "app_server" {
   name              = var.instance_name
   availability_zone = var.availability_zone
   blueprint_id      = "ubuntu_22_04"
-  bundle_id         = "nano_2_0"
+  bundle_id         = "small_2_0"
   key_pair_name     = var.key_pair_name
+  user_data         = file("${path.module}/docker-setup.sh")
 
   tags = {
     Name = var.instance_name

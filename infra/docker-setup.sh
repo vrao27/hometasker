6c06705e84390d9f52725472c137248106afc4e8
Vinay Rao <vrao8912@gmail.com>
2025-07-26 10:51:26 +0200

    fix mongo db uri

--- infra/docker-setup.sh
+++ infra/docker-setup.sh
@@ -60,7 +60,7 @@ chown ubuntu:ubuntu hometasker/.env
 cat <<EOF > hometasker/backend/.env
 PORT=5000
 NODE_ENV=development
-MONGO_URI=mongodb://localhost:27017/hometaskerDB
+MONGO_URI=mongodb://db:27017/hometaskerDB
 TOKEN_SECRET=$(head -c32 /dev/urandom | base64)
 EOF
 chown ubuntu:ubuntu hometasker/backend/.env

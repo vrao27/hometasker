10d46b9aea8f5ac3ad830046a153c335c62bc338
Vinay Rao <vrao8912@gmail.com>
2025-07-27 17:08:03 +0200

    fix the plugin issue for docker compose

--- infra/cloud-init.sh
+++ infra/cloud-init.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 sudo apt-get update
-sudo apt-get install -y docker.io git docker-compose-plugin
+sudo apt-get install -y docker.io git 
+
+DOCKER_CONFIG=${DOCKER_CONFIG:-/home/ubuntu/.docker}
+sudo mkdir -p $DOCKER_CONFIG/cli-plugins
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
 sudo usermod -aG docker ubuntu
 

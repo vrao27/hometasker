d602b489804b762161038e92601161311cfd1bce
Vinay Rao <vrao8912@gmail.com>
2025-07-27 22:16:40 +0200

    docker compose plugin fix insallation code

--- infra/cloud-init.sh
+++ infra/cloud-init.sh
@@ -3,10 +3,9 @@ set -e
 sudo apt-get update
 sudo apt-get install -y docker.io git 
 
-DOCKER_CONFIG=${DOCKER_CONFIG:-/home/ubuntu/.docker}
-sudo mkdir -p $DOCKER_CONFIG/cli-plugins
-sudo curl -SL https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+sudo mkdir -p /usr/lib/docker/cli-plugins
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-linux-x86_64 -o /usr/lib/docker/cli-plugins/docker-compose
+sudo chmod +x /usr/lib/docker/cli-plugins/docker-compose
 
 sudo usermod -aG docker ubuntu
 
